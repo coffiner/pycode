0b682b0f40c1b8851a50625de1bcc1613a8826f9
kmean <cangmean@gmail.com>
2013-09-25 17:07:46 +0800

    delete myblog ver1

--- myblog/static/js/style.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-		$('.cat-item').mouseenter(function(){
-			$(this).animate({paddingLeft: "6px"},"5");
-		}).mouseout(function(){
-			$(this).animate({paddingLeft: "0px"}, "1");
-		})
-	})
